967f88ba0185493d50dbe151fb23d3cc91c2a6b8
jo-taichi <strarf2215@yahoo.co.jp>
2020-06-23 15:32:40 +0900

    modifying1

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,6 +2,11 @@ class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id]
-    @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+    if last_message_id == nil
+      @messages = group.messages.includes(:user)
+    else
+      @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+    end
   end
 end
+
